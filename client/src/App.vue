1c8eac0cdcb0371919ca36d4d8d8e4f7e465471f
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-02-27 22:33:43 +0400

    feature-1: add first block

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-
+import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-Hello
-    </div>
-  </header>
-  <main>
+  <main class="min-h-screen">
+    <Hero />
 
+    <section id="content" class="py-16">
+      <div class="mx-auto max-w-3xl px-4">
+        <div class="alert alert-info">
+          <span>Дальше добавим: причины, галерею, таймлайн и письмо 💌</span>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
-
-<style scoped>
-
-</style>
