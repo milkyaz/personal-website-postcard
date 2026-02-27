942ba46792866f73a15dc16255cb0c6e41785f6c
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-02-27 22:23:41 +0400

    main: install dependencies

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+Hello
+    </div>
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
